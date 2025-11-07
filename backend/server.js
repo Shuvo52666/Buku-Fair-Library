e6df0ed0b41af29fe0a46707b33c576f9a19af27
Shuvo <5481shuvo@gmail.com>
2025-11-08 01:00:37 +0600

    7/11/2025

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import Book from './models/book.js';
 
 // connect mongodb// bukufairlibrary // 5481shuvo_db_user
 // mongodb+srv://5481shuvo_db_user:bukufairlibrary@cluster0.3y5escz.mongodb.net/bookStore?appName=Cluster0
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "5481shuvo_db_user:bukufairlibrary@cluster0.3y5escz.mongodb.net/bookStore?appName=Cluster0";
 ;
 await mongoose.connect(MONGO_URI)
 .then(
@@ -45,13 +45,13 @@ app.get("/allbooks",async(req,res)=>{
   res.json(abooks);
 })
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
 
 
 
-// app.listen(5000, () => console.log("Backend running on port 5000"));
+app.listen(5000, () => console.log("Backend running on port 5000"));
 
 
 
