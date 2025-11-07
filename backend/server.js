459cf397a7a3f684f3ee10d41e585612bd57cd1c
Shuvo <5481shuvo@gmail.com>
2025-11-08 02:28:45 +0600

    final

--- backend/server.js
+++ backend/server.js
@@ -45,13 +45,13 @@ app.get("/allbooks",async(req,res)=>{
   res.json(abooks);
 })
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
 
 
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+// app.listen(5000, () => console.log("Backend running on port 5000"));
 
 
 
