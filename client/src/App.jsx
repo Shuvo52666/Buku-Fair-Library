9041e8d8a8d2515a15257b04a8ea040fdd4dc3bd
Shuvo <5481shuvo@gmail.com>
2025-11-09 15:45:32 +0600

    hello

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,10 @@ import Admin from './Admin';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
