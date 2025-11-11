69189798fcec93dc5d328218a63a6ebb3bb2a274
Shuvo <5481shuvo@gmail.com>
2025-11-12 02:10:42 +0600

    12/112025

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 
 const bookSchema = new mongoose.Schema({
-    URL:String,
+    URL:{type:String,required:true},
     name: {type:String,required:true},
     author: {type:String,required:true},
-    status: String,
-    price:String,
-    keywords:String,
+    status: {type:String,required:true},
+    price:{type:String,required:true},
+    keywords:{type:String,required:true},
 });
 
 const Book = mongoose.model("Book", bookSchema);
